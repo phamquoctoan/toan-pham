24742be79c0c006f7a436fca3d16b0e78e91d4ea
phamquoctoan <phamquoctoan@hotmail.com>
2016-06-19 13:49:23 +0700

    Add posts model & controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160607152339) do
+ActiveRecord::Schema.define(version: 20160619064215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "articles", force: :cascade do |t|
+  create_table "posts", force: :cascade do |t|
     t.string   "title"
-    t.text     "body"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
